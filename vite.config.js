f00aa68dbe538cb7a1d258beb9818cec5127fc92
Kumari Vijeta <kumarivijeta2110@gmail.com>
2025-03-24 21:03:27 +0530

    fix(versionupdate): removed carbon reference and updated shidoka  version

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,9 @@
 import { defineConfig } from 'vite';
+import ViteSvgr from 'vite-plugin-svgr';
 import { svelte, vitePreprocess } from '@sveltejs/vite-plugin-svelte';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [svelte()],
+  plugins: [svelte(),ViteSvgr()],
   preprocess: [vitePreprocess()],
 });
